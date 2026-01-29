48e9cc93bf393ed4ece571de2663e7d9936a05f3
Christian Helle <christian.helle@outlook.com>
2026-01-29 22:31:13 +0100

    Disable auto-format and auto launching of neo-tree

--- lua/plugins/animate.lua
+++ lua/plugins/animate.lua
@@ -1,6 +1,20 @@
 return {
+  {
     "snacks.nvim",
     opts = {
       scroll = { enabled = false },
     },
-  }
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    init = function()
+      -- do nothing
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      autoformat = false,
+    },
+  },
+}
